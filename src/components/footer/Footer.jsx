4b9990a6f640d6c10c31c5bf2c817b46a655b67a
OrangeBirdDrinksWater <114991717+OrangeBirdDrinksWater@users.noreply.github.com>
2023-11-12 20:16:13 +0800

    init
    don't forget link should be inside of head under html

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="container">
+      Copyright Notice Privacy Policy Link Sitemap Logo Contact Information
+      Social Media Icons Email Sign-up Form
+    </div>
+  );
+};
+
+export default Footer;
