9fcebc7e5f8b6ebf6ef4005833aac6dc322c5e59
OrangeBirdDrinksWater <114991717+OrangeBirdDrinksWater@users.noreply.github.com>
2023-11-21 14:45:39 +0800

    a little patch
    a little patch

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -14,6 +14,7 @@ const LoginPage = () => {
 
   if (status === "authenticated") {
     router.push("/");
+    console.log("You are Authenticated!")
   }
 
   return (
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,9 +1,8 @@
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
 import { getServerSession } from "next-auth";
-
-const prisma = new PrismaClient();
+import prisma from "./connect";
 
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
@@ -13,16 +12,34 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      profile(profile) {
-        return {
-          // Return all the profile information you need.
-          // The only truly required field is `id`
-          // to be able identify the account when added to a database
-        };
+    }),
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
       },
     }),
   ],
 };
-
 
 export const getAuthSession = getServerSession();
